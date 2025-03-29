d09c2b138e8aa44f30fae1bedeeb4ddef4fa0b5e
Error <150291023+Hero-Error@users.noreply.github.com>
2025-03-29 11:01:15 +0600

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ for _ in range(reload_count):
         print(f"Button not found, skipping click. Error: {e}")
 
     # Wait for 3 minutes before refreshing
-    time.sleep(‭‭60000)
+    time.sleep(‭‭120)
 
     # Refresh the webpage
     driver.refresh()
